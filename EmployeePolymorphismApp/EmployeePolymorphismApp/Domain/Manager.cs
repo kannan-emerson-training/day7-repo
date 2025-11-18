ee39f242b496a52051bfa330b13b17cb93e3a0d6
kannan <kannans89@hotmail.com>
2025-11-18 11:04:51 +0530

    role

---
+++ EmployeePolymorphismApp/EmployeePolymorphismApp/Domain/Manager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePolymorphismApp.Domain
+{
+    class Manager : Employee
+    {
+        private double _hra;
+        private double _ta;
+        private double _da;
+        public Manager(int id, string name, double basicSalary) 
+            : base(id, name, basicSalary)
+        {
+            _hra = .50*_basicSalary;
+            _ta= .40*_basicSalary;
+            _da= .30*_basicSalary;
+        }
+
+        public override double CalculateTotalSalary()
+        {
+            return _basicSalary + _ta + _da+_hra;
+        }
+        public override string ToString()
+        {
+            return "Manager";
+        }
+
+        public override string Role {
+            get {
+                return "Manager ".ToUpper();
+             }
+        }
+    }
+}
---
+++ EmployeePolymorphismApp/EmployeePolymorphismApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using EmployeePolymorphismApp.Domain;
+
+namespace EmployeePolymorphismApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var developer = new Developer(1001, "Hemang", 20000);
+            var manager = new Manager(2001, "Ashish", 50000);
+
+            PrintSalarySlip(developer);
+            PrintSalarySlip(manager);
+
+        }
+
+       static void PrintSalarySlip(Employee employee)
+        {
+            //Console.WriteLine("job is :"+employee.GetType().Name);
+            Console.WriteLine("toString Role :"+employee.ToString());
+            Console.WriteLine("Role :" + employee.Role);
+            Console.WriteLine("id " + employee.Id);
+            Console.WriteLine("name " + employee.Name);
+            Console.WriteLine("basic " + employee.BasicSalary);
+            Console.WriteLine("Total salry " + employee.CalculateTotalSalary());
+        }
+    }
+}
